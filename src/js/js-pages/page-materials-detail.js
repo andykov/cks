9686ba8869eb1ffc4fd7972a687995a27a511376
Andrey Budnikov <andykov.work@gmail.com>
2019-04-15 06:07:36 +0300

    global update

--- src/js/js-pages/page-materials-detail.js
+++ src/js/js-pages/page-materials-detail.js
@@ -1,3 +1,29 @@
+//=require ../libs/jquery.sliderPro.js
+
+
 $(function(){
-    $('.js-media-cont').sliderPro();
+    $('.js-gallery').sliderPro({
+        width: 1260,
+        height: 612,
+        // orientation: 'vertical',
+        loop: false,
+        arrows: true,
+        buttons: true,
+        thumbnailsPosition: 'right',
+        thumbnailPointer: true,
+        thumbnailWidth: 160,
+        breakpoints: {
+            800: {
+                thumbnailsPosition: 'bottom',
+                thumbnailWidth: 160,
+                thumbnailHeight: 72
+            },
+            500: {
+                // orientation: 'vertical',
+			    thumbnailsPosition: 'bottom',
+                thumbnailWidth: 160,
+                thumbnailHeight: 72
+            }
+        }
+    });
 });
